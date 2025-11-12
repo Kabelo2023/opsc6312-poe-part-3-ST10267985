9c0460a57efc736629e9e0a4413eda4ddcf6a21f
GaryRambuda <garyrambuda@gmail.com>
2025-11-12 13:09:06 +0200

    build(gradle): enable Java 17 toolchain auto-download + fix settings script order

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     id("com.android.application") version "8.9.2" apply false
     id("com.android.library") version "8.9.2" apply false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+// ← plugins block must come AFTER pluginManagement
+plugins {
+    // lets Gradle download JDKs (Java toolchains) automatically
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
